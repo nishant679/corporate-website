917cd8fee5f5954116d5ffbed4a781d38f6cf6af
Shrey Gupta <connectwithshrey@gmail.com>
2019-10-01 00:57:37 +0530

    Redundant Css

--- client/src/pages/newsroom/Top.js
+++ client/src/pages/newsroom/Top.js
@@ -6,61 +6,63 @@ function Top() {
     return (
         <React.Fragment>
         <div className="container-fluid" style={{marginTop: '25vh'}}>
-            <div className="row" style={{marginLeft: '170px', paddingBottom: 0}} >
-                <div className="offset-md-6 col-md-5" style={{display: 'flex', justifyContent: 'flex-end', padding: 0}}>
-                    <div  className="rectangle" style={{background: 'rgba(212,212,212,1)'}} />
-                </div>
-                <div className="col-md-1"></div>
-            </div>
-            <div className="row main">
-                <div className="col-md-7 col-s-8 maintext">
-                    Tech has no borders <br/> Neither should you
-                    <div className="intro">
-                        We make tech and market opportunities accessible to everyone by<br/> breaking down borders.
-                        Through us companies expand their reach to <br/>the opposite side of the world, thereby
-                        tethering
-                        their growth to the rise<br/> of developing countries via transparent aggregation.<br/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-7 maintext">
+                        Tech has no borders <br/> Neither should you
+                        <div className="intro">
+                            We make tech and market opportunities accessible to everyone by<br/> breaking down borders.
+                            Through us companies expand their reach to <br/>the opposite side of the world, thereby
+                            tethering
+                            their growth to the rise<br/> of developing countries via transparent aggregation.<br/>
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-5 col-sm-4">
-                    <div className="row">
-                        <div className="col-md-5">
-                            <div className="rectangle" style={{background: 'rgba(212,212,212,1)'}} />
+                    <div className="col-md-5 col-sm-4">
+                        <div className="row" >
+                            <div className="col-md-6" style={{padding: 0}}>
+                                <div className="rectangle" style={{background: 'rgba(212,212,212,1)'}} />
+                            </div>
+                            <div className="col-md-6" style={{padding: 0}}>
+                                <div className="rectangle" style={{background: 'rgba(233,232,232,1)'}} />
+                            </div>
                         </div>
-                        <div className="col-md-5" style={{padding: 0}}>
-                            <div className="rectangle" style={{background: 'rgba(233,232,232,1)'}} />
+                        <div className="row">
+                            <div className="col-md-6" style={{padding: 0}}>
+                                <div className="rectangle" style={{background: 'rgba(233,232,232,1)'}} />
+                            </div>
+                            <div className="col-md-6" style={{padding: 0}}>
+                                <div className="rectangle" style={{background: 'rgba(212,212,212,1)'}} />
+                            </div>
                         </div>
-                        <div className="col-md-2"></div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-5">
-                            <div className="rectangle" style={{background: 'rgba(233,232,232,1)'}} />
-                        </div>
-                        <div className="col-md-5" style={{padding: 0}}>
-                            <div className="rectangle" style={{background: 'rgba(212,212,212,1)'}} />
-                        </div>
-                        <div className="col-md-2"></div>
                     </div>
                 </div>
             </div>
-            <div className="row" style={{backgroundColor: 'rgba(245,245,245,1)'}}>
-                    <div className="offset-md-1 col-md-2">
+        </div>
+
+            <div style={{backgroundColor: 'rgba(245,245,245,1)'}}>
+            <div className="container">
+            <div className="row" style={{width: '100%'}}>
+                    <div className="col-md-2" style={{paddingLeft: 0}}>
                         <div className="rectangle-big" style={{background: 'rgba(212,212,212,1)'}} />
                     </div>
-                    <div className="col-md-2">
+                    <div className="col-md-2" style={{paddingLeft: 0}}>
                         <div className="rectangle-big" style={{background: 'rgba(225,225,225,1)'}} />
                     </div>
-                    <div className="col-md-2">
+                    <div className="col-md-2" style={{paddingLeft: 0}}>
                         <div className="rectangle-big" style={{background: 'rgba(245,245,245,1)'}} />
                     </div>
-                    <div className="col-md-2">
+                    <div className="col-md-2" style={{paddingLeft: 0}}>
                         <div className="rectangle-big" style={{background: 'rgba(225,225,225,1)'}} />
                     </div>
-                    <div className="col-md-2">
+                    <div className="col-md-2" style={{paddingLeft: 0}}>
                         <div className="rectangle-big" style={{background: 'rgba(212,212,212,1)'}} />
+                    </div>
+                    <div className="col-md-2" style={{paddingLeft: 0}}>
+                        <div className="rectangle-big" style={{background: 'rgba(225,225,225,1)'}} />
                     </div>
 
                 {/*<div style={{height: '5vh'}}>&nbsp;</div>*/}
+            </div>
             </div>
             </div>
             <div style={{backgroundColor: 'rgba(245,245,245,1)'}}>
